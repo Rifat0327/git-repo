cbfbb4d4bfe67588189b57aebe2fd33c50a37146
Rifat <rifat0327@mail.ru>
2025-01-24 12:48:36 +0300

    add auto

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const obj = {
-   brand: 'AUDI',
-   model: 'SQ8',
-   color: "yellow",
-}
+  brand: "AUDI",
+  model: "SQ8",
+  color: "yellow",
+};
+
+const auto = {
+  brand: "Subaru",
+  model: "Impreza WRX STI",
+  year: 2015,
+};
